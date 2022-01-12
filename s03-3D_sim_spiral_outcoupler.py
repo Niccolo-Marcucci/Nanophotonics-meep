8922a1f9593fa7e49664e6676047f750c71f717e
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2022-01-12 12:12:04 +0100

    change main object to subclass of meep.Simulation

--- s03-3D_sim_spiral_outcoupler.py
+++ s03-3D_sim_spiral_outcoupler.py
@@ -40,7 +40,7 @@ def convert_seconds (elapsed):
 
 
 
-class Simulation():
+class Simulation(mp.Simulation):
 
     def __init__(self, sim_name='simulation_buried', dimensions=3, symmetries = [], empty = False):
 
@@ -56,7 +56,7 @@ class Simulation():
 
         self._empty = empty
 
-        self.sim = mp.Simulation(
+        super().__init__(
                     cell_size = mp.Vector3(1,1,1),
                     geometry = [],
                     sources = [],
@@ -77,15 +77,15 @@ class Simulation():
         self._empty = value
 
         if self._empty :
-            self.sim.geometry = []
+            self.geometry = []
         else:
-            self.sim.geometry = self.geometry
+            self.geometry = self._geometry
 
-        self.sim.reset_meep()
+        self.reset_meep()
 
     def init_geometric_objects(self, multilayer_file, D=5, grating_period=0.2, N_rings=10, N_arms=0, lambda_bsw=0.4,
                                pattern_type = 'positive') :
-        self.geometry = []
+        self._geometry = []
 
         self.domain_x = self.domain_y = grating_period*N_rings*2 + D + self.extra_space_xy*2
 
@@ -98,7 +98,7 @@ class Simulation():
                         exclude_last_layer = False,
                         buried = True)
         print(design_specs)
-        self.geometry.extend(multilayer)
+        self._geometry.extend(multilayer)
 
         if pattern_type == 'positive':
             grating_index = np.real(design_specs['idx_layers'][-3])
@@ -108,7 +108,7 @@ class Simulation():
                                                           self.domain_y+.5 + 2*self.PML_width,
                                                           design_specs['d_layers'][-3]),
                                     center   = mp.Vector3(0, 0, 0))
-            self.geometry.append(dummy_layer)
+            self._geometry.append(dummy_layer)
 
         elif pattern_type == 'negative':
             grating_index = np.real(design_specs['idx_layers'][-2])
@@ -127,7 +127,7 @@ class Simulation():
                         thickness = design_specs['d_layers'][-3],
                         orientation = mp.Vector3(0, 0, 1))
 
-        self.geometry.extend(outcoupler)
+        self._geometry.extend(outcoupler)
 
         beam_block = mp.Cylinder(
                         center = mp.Vector3(0, 0, self.z_top_air_gap-0.1),
@@ -136,19 +136,19 @@ class Simulation():
                         material = mp.metal)
         beam_block.name = 'Beam_block'
 
-        self.geometry.append(beam_block)
+        self._geometry.append(beam_block)
 
         if not self.empty:
-            self.sim.geometry = self.geometry
+            self.geometry = self._geometry
 
         self.domain_z = self.substrate_thickness + multilayer_thickness + self.z_top_air_gap
 
                            # resolution is 10 points per wavelength in the highest index material time a scale factor
-        self.sim.resolution = int(10/(1/f/np.real(np.max(design_specs['idx_layers']))) * 1.5)
-        print(1/self.sim.resolution)
+        self.resolution = int(10/(1/f/np.real(np.max(design_specs['idx_layers']))) * 1.5)
+        print(1/self.resolution)
 
         # round domain with an integer number of grid points
-        self.grid_step = 1/self.sim.resolution
+        self.grid_step = 1/self.resolution
         # self.domain_x = int(self.domain_x/self.grid_step) * self.grid_step
         # self.domain_y = int(self.domain_y/self.grid_step) * self.grid_step
         # self.PML_width = int(self.PML_width/self.grid_step) * self.grid_step
@@ -156,17 +156,16 @@ class Simulation():
         # print(self.grid_step)
         # self.domain_z = int(self.domain_z/self.grid_step) * self.grid_step
 
-        self.sim.cell_size = mp.Vector3(self.domain_x + 2*self.PML_width,
+        self.cell_size = mp.Vector3(self.domain_x + 2*self.PML_width,
                                         self.domain_y + 2*self.PML_width,
                                         self.domain_z + 2*self.PML_width)
 
-        self.sim.geometry_center = mp.Vector3(0, 0, -(self.sim.cell_size.z/2 - self.z_top_air_gap - self.PML_width - design_specs['d_layers'][-2] - design_specs['d_layers'][-3]/2))
+        self.geometry_center = mp.Vector3(0, 0, -(self.cell_size.z/2 - self.z_top_air_gap - self.PML_width - design_specs['d_layers'][-2] - design_specs['d_layers'][-3]/2))
 
-        self.sim.boundary_layers = [mp.PML(self.PML_width)]
-        # print( [self.sim.cell_size.x / self.sim.
+        self.boundary_layers = [mp.PML(self.PML_width)]
+        # print( [self.cell_size.x / self.
 
     def init_sources_and_monitors(self, f, df, use_monitor=True) :
-        self.sim.sources = []
 
         if df == 0 :
             source = mp.Source(mp.ContinuousSource(f,width=0.1 ),
@@ -177,7 +176,7 @@ class Simulation():
                                component=mp.Ez,
                                center=mp.Vector3() )
 
-        self.sim.sources.append(source)
+        self.sources.append(source)
 
         monitor_distance  = self.z_top_air_gap - 0.03
 
@@ -193,7 +192,7 @@ class Simulation():
         # #                       size=mp.Vector3(0,0,0),
         # #                       direction=mp.Z)
 
-        self.monitors.append(self.sim.add_near2far(f, 0, 1, nearfield) if use_monitor else None)#, yee_grid=True))
+        self.monitors.append(self.add_near2far(f, 0, 1, nearfield) if use_monitor else None)#, yee_grid=True))
 
     # def create_blender_primitive(self, scale_factor=1):
     #     blend_name = f"{self.name}.blendtxt"
@@ -259,13 +258,13 @@ class Simulation():
                                        obj.center.z*scale_factor])
                 scad.write(scad_name, mode='a')
 
-            sim_domain = ops.Cube([(self.sim.cell_size.x - self.PML_width) * scale_factor,
-                                    (self.sim.cell_size.y - self.PML_width) * scale_factor,
-                                    (self.sim.cell_size.z - self.PML_width) * scale_factor], center = True)
+            sim_domain = ops.Cube([(self.cell_size.x - self.PML_width) * scale_factor,
+                                    (self.cell_size.y - self.PML_width) * scale_factor,
+                                    (self.cell_size.z - self.PML_width) * scale_factor], center = True)
             sim_domain = sim_domain.color([.5, .5, .5, .5])
-            sim_domain = sim_domain.translate([self.sim.geometry_center.x*scale_factor,
-                                                self.sim.geometry_center.y*scale_factor,
-                                                self.sim.geometry_center.z*scale_factor])
+            sim_domain = sim_domain.translate([self.geometry_center.x*scale_factor,
+                                                self.geometry_center.y*scale_factor,
+                                                self.geometry_center.z*scale_factor])
 
             # sim_domain.write(scad_name, mode='a')
 
@@ -279,16 +278,6 @@ class Simulation():
             #                               center.z*scale_factor])
             #     plane.write(scad_name, mode='a')
 
-
-    # define aliases
-    def run(self,*args,**kwargs):
-        self.sim.run(*args,**kwargs)
-
-    def init_sim(self):
-        self.sim.init_sim()
-
-    def round_time(self):
-        self.sim.round_time()
 
 
 #%% geometry and simulation parameters
@@ -310,8 +299,8 @@ n_eff = n_eff_h*.7 + n_eff_l*.3
 pattern_type = 'positive'
 
 outcoupler_period = 2*round(wavelength/(n_eff_l+n_eff_h),3)
-N_periods = 5
-D = 5
+N_periods = 1
+D = 1
 charge = 1
 
 t0 = time.time()
@@ -343,32 +332,32 @@ sim.init_sim()
 # raise ValueError()
 
 date = time.strftime('%y%m%d-%H%M%S')#'211001-121139'#
-sim_suffix = f'res{sim.sim.resolution}_{date}'
+sim_suffix = f'res{sim.resolution}_{date}'
 
 print(f'\n\nSimulation took {convert_seconds(time.time()-t0)} to initiate\n')
 #%%
-simsize = sim.sim.cell_size
-center  = sim.sim.geometry_center
+simsize = sim.cell_size
+center  = sim.geometry_center
 
 
 # # # plt.figure(dpi=200)
 # fig = plt.figure(dpi=300)
 # ax1 = fig.add_subplot(1, 2, 1)
-# sim.sim.plot2D( output_plane=mp.Volume(center=center,size=mp.Vector3(0,simsize.y,simsize.z)),
+# sim.plot2D( output_plane=mp.Volume(center=center,size=mp.Vector3(0,simsize.y,simsize.z)),
 #                 labels=True,
 #                 eps_parameters={"interpolation":'none',"cmap":'gnuplot'} )
 # ax2 = fig.add_subplot(1, 2, 2)
-# sim.sim.plot2D( ax=ax2, output_plane=mp.Volume(size=mp.Vector3(simsize.x,simsize.y)),
+# sim.plot2D( ax=ax2, output_plane=mp.Volume(size=mp.Vector3(simsize.x,simsize.y)),
 #                 labels=True,
 #                 eps_parameters={"interpolation":'none',"cmap":'gnuplot'})
 # fig.savefig(f'{sim_name}-{sim_suffix}_section.jpg')
 # # plt.close()
 # plt.show()
 # plt.show(block=False)
-# sim.sim.output_epsilon(f'{sim_name}_eps')
-# eps_data = sim.sim.get_epsilon()
+# sim.output_epsilon(f'{sim_name}_eps')
+# eps_data = sim.get_epsilon()
 # mpo.savemat(f'{sim_name}_eps.mat', {"eps_data": eps_data})
-# x, y, z, w = [np.array(tmp) for tmp in sim.sim.get_array_metadata()]
+# x, y, z, w = [np.array(tmp) for tmp in sim.get_array_metadata()]
 # mpo.plot_image(z, y, eps_data[:,:,84], vmax=9.0, vmin=1.0)
 # mpo.plot_image(y, z, eps_data[int(eps_data.shape[0]/2)+1,:,:])#, vmax=9.0, vmin=-1.0)
 
@@ -385,14 +374,14 @@ def print_time(sim):
 t0 = time.time()
 mp.verbosity(1)
 
-sim.run(mp.at_every(1,print_time),until=30)
+sim.run(mp.at_every(1,print_time),until=.30)
 # sim.run(until_after_sources=mp.stop_when_fields_decayed(1, mp.Ez, mp.Vector3(), sim_end))
 
-t = np.round(sim.sim.round_time(), 2)
+t = np.round(sim.round_time(), 2)
 
-sim.sim.save_near2far(near2far=sim.monitors[0], fname=f'{sim_suffix}_nearfield_t{t}')
+sim.save_near2far(near2far=sim.monitors[0], fname=f'{sim_suffix}_nearfield_t{t}')
 
-ex_near, ey_near = [sim.sim.get_dft_array(sim.monitors[0], field, 0) for field in [mp.Ex, mp.Ey]]
+ex_near, ey_near = [sim.get_dft_array(sim.monitors[0], field, 0) for field in [mp.Ex, mp.Ey]]
 
 mpo.savemat(f'{sim_name}-{sim_suffix}_nearfield_t{t}.mat', {'Ex': ex_near, 'Ey': ey_near,
                                                             'Lx': sim.monitors[0].regions[0].size.x,
@@ -403,10 +392,10 @@ print(f'\n\nSimulation took {convert_seconds(time.time()-t0)} to run\n')
 t1 = time.time()
 
 r = 1e6 #1m
-n_freq = 120
+n_freq = 3
 res = n_freq/(r/3)
 
-fields = sim.sim.get_farfields(near2far=sim.monitors[0], resolution=res, center=mp.Vector3(0,0,r), size=mp.Vector3(r/3,r/3,0))
+fields = sim.get_farfields(near2far=sim.monitors[0], resolution=res, center=mp.Vector3(0,0,r), size=mp.Vector3(r/3,r/3,0))
 Ex, Ey, Ez = [ fields[k] for k in ['Ex', 'Ey', 'Ez']]
 mpo.savemat(f'{sim_name}-{sim_suffix}_farfield_t{t}.mat', fields)
 
@@ -414,7 +403,7 @@ print(f'\n\nFar field took {convert_seconds(time.time()-t1)} to compute\n')
 
 del ex_near, ey_near, fields, Ex, Ey, Ez
 plt.close()
-sim.sim.reset_meep()
+sim.reset_meep()
 
 sim = Simulation(sim_name)
 
@@ -431,10 +420,10 @@ mp.verbosity(1)
 sim.init_sim()
 
 f = plt.figure(dpi=150)
-Animate = mp.Animate2D(sim.sim, fields=mp.Hy, f=f, realtime=False, normalize=True,
+Animate = mp.Animate2D(sim, fields=mp.Hy, f=f, realtime=False, normalize=True,
                         output_plane=mp.Volume(center=center,size=mp.Vector3(0,simsize.y,simsize.z)))
 
-sim.run(mp.at_every(0.1,Animate),until=20)
+sim.run(mp.at_every(0.1,Animate),until=.2)
 
 filename = f'{sim_name}-{sim_suffix}_section.mp4'
 Animate.to_mp4(10,filename)
