46afcfa01c284c4ad4d06d8ae4c0c20b115f2089
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2022-01-13 14:42:42 +0100

    jobname as second argument of the bash script

--- s06-gitcommit_n_sbatch_run.sh
+++ s06-gitcommit_n_sbatch_run.sh
@@ -2,11 +2,18 @@
 
 filename="$1"
 
+if [ $# -gt 1 ]
+then
+    jobname="$2"
+else
+    jobname="meep_sim"
+fi
+
 previous_hash=$(git log -n 1 --pretty=format:"%H")
 prefix="${previous_hash:0:10}"
 
 git add $filename
 git add s06-sbatch_parallel_exec.sbatch
-git commit -m "cluster run, simulation $filename. The prefix for the simulation is the hash of the previous commit"
+git commit -m "cluster run: job $jobname, file $filename, prefix $prefix. The prefix for the simulation is the hash of the previous commit"
 
-sbatch s06-sbatch_parallel_exec.sbatch $filename $prefix
+sbatch --job-name=$jobname --output="$jobname_$prefix_%j.log" --error="$jobname_$prefix_%j.err" s06-sbatch_parallel_exec.sbatch $filename $prefix
