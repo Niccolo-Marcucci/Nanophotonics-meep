6a5817b4a78a250b21b91858208e0dcbcc4848bb
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2022-01-19 00:45:13 +0100

    cluster run: job cavity_30_high_res, file s01-3D_cavity_outcoupler_on_multilayer.py. The prefix for the simulation is the hash of this commit

--- s01-3D_cavity_outcoupler_on_multilayer.py
+++ s01-3D_cavity_outcoupler_on_multilayer.py
@@ -381,7 +381,7 @@ if __name__ == "__main__":              # good practise in parallel computing
 
     sim = Simulation(sim_name)
     sim.extra_space_xy += wavelength/n_eff_l * (charge > 0)
-    sim.eps_averaging = True
+    # sim.eps_averaging = True
 
     sim.init_geometric_objects( multilayer_file = f"./Lumerical-Objects/multilayer_design/designs/{file}",
                                 used_layer = -3 if buried else -2,
