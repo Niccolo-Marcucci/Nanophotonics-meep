4b3bf310a75997b625cb7750d8cfb1cdd2614e58
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2022-01-21 01:06:16 +0100

    cluster run: job cavity_and_out, file s01-3D_cavity_outcoupler_on_multilayer.py. The prefix for the simulation is the hash of this commit

--- s01-3D_cavity_outcoupler_on_multilayer.py
+++ s01-3D_cavity_outcoupler_on_multilayer.py
@@ -393,7 +393,7 @@ if __name__ == "__main__":              # good practise in parallel computing
 
     sim.init_geometric_objects( multilayer_file = f"./Lumerical-Objects/multilayer_design/designs/{file}",
                                 used_layer = -3 if buried else -2,
-                                res_scaling = .1,
+                                res_scaling = 2,
                                 use_BB = False,
                                 pattern_type = pattern_type,
                                 cavity_parameters = cavity_parameters,
@@ -480,7 +480,7 @@ if __name__ == "__main__":              # good practise in parallel computing
         step_functions.append( mp.after_sources(sim.harminv_instance) )
 
 
-    sim.run(*step_functions, until=50)#_after_sources=mp.stop_when_fields_decayed(1, mp.Ez, mp.Vector3(), 1e-1))
+    sim.run(*step_functions, until=500)#_after_sources=mp.stop_when_fields_decayed(1, mp.Ez, mp.Vector3(), 1e-1))
     # sim.run(until_after_sources=mp.stop_when_dft_decayed(minimum_run_time=10))
 
     print(f'\n\nSimulation took {convert_seconds(time.time()-t0)} to run\n')
