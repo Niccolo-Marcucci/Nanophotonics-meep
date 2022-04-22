9ec70acf51d2dea6e76f57a20d00350f9ff4ef5f
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2022-04-22 18:51:07 +0200

    to be finished

--- s09-gitcommit_n_many_sbatch.sh
+++ s09-gitcommit_n_many_sbatch.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
 filename="$1"
-echo $filename
 
-jobname="meep_sim"
+if [ $# -gt 2 ]
+then
+    empty="_$3"
+else
+    empty=""
+fi
+if [ $# -gt 1 ]
+then
+    jobname="$2"
+else
+    jobname="meep_sim"
+fi
+
 
 git add $filename
-git commit -m "local run: file $filename. The prefix for the simulation is the hash of this commit"
+git add s06-sbatch_parallel_exec.sbatch
+git commit -m "cluster run: job $jobname, file $filename. The prefix for the simulation is the hash of this commit"
 
 hash=$(git log -n 1 --pretty=format:"%H")
 prefix="${hash:0:10}"
 
 output_folder="${jobname}_$(date +%y%m%d-%H%M%S)_${prefix}"
 
+outputname="${jobname}${empty}_${prefix}_%j.log"
+errorname="${jobname}${empty}_${prefix}_%j.err"
+
+jobname="${jobname}${empty}_${prefix}"
 prefix="$(date +%y%m%d-%H%M%S)_${prefix}"
 
 mkdir -p data/$output_folder
+
+echo "$outputname, $errorname"
 
 njobs=$2
 
 for (( i=0 ; i<$2 ; i++ ));
 do
-    python $filename ${prefix} "parallel_bash" $i $njobs 
+    sbatch --job-name=$jobname --output=$outputname --error=$errorname s09-sbatch_single_exec.sbatch $filename $prefix "parallel_bash" $i $njobs
 done
-
-wait
-mv *${prefix}* data/$output_folder
