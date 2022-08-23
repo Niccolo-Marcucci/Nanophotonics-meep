6f7e3af53668d34c95d6d63c9045c7947c04427e
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2022-08-24 01:29:11 +0200

    local run: file s11-2D_circular_Anisotropic_DBR_cavity.py. The prefix for the simulation is the hash of this commit

--- s11-2D_circular_Anisotropic_DBR_cavity.py
+++ s11-2D_circular_Anisotropic_DBR_cavity.py
@@ -449,8 +449,8 @@ if __name__ == "__main__":              # good practise in parallel computing
                     0 )]
     empty = False
 
-    j = 0
-    tuple_list = []
+    j = 1
+    # tuple_list = []
     for source_pos in [0]: # 0, period/4, period/2]:
     #     for n_eff_h in n_eff_hs :
     #         for D in Ds:
