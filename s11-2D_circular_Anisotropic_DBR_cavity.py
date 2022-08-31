e8a05f22011f6f6d9123a4efd07b3932816250db
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2022-08-31 18:18:19 +0200

    cluster run: job azimutal_anis_dispersed_many_angles, file s11-2D_circular_Anisotropic_DBR_cavity.py. The prefix for the simulation is the hash of this commit

--- s11-2D_circular_Anisotropic_DBR_cavity.py
+++ s11-2D_circular_Anisotropic_DBR_cavity.py
@@ -524,8 +524,8 @@ if __name__ == "__main__":              # good practise in parallel computing
     # j = 0           # resets  tiple list (insted of commenting all previous lines)
     # tuple_list = []
 
-    for source_tilt in np.linspace(-np.pi/2,+np.pi/2,32)[1:]:
-        for wavelength in np.linspace(.550, .630, 200):
+    for source_tilt in np.linspace(-np.pi/2,+np.pi/2,13)[1:]:
+        for wavelength in np.linspace(.565, .615, 150):
             n_eff_h      = n_eff([31e-9, wavelength*1e-6])[0]
             n_eff_l      = n_eff([ 2e-9, wavelength*1e-6])[0]
             n_eff_mod_l  = n_eff([15e-9, wavelength*1e-6])[0] - n_eff([ 2e-9, wavelength*1e-6])[0]
