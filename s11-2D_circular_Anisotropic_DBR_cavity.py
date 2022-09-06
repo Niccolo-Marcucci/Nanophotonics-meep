4bfddc7df46de0e94df6aac21ca073940e3869cc
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2022-09-06 11:40:48 +0200

    local run: file s11-2D_circular_Anisotropic_DBR_cavity.py. The prefix for the simulation is the hash of this commit

--- s11-2D_circular_Anisotropic_DBR_cavity.py
+++ s11-2D_circular_Anisotropic_DBR_cavity.py
@@ -245,7 +245,7 @@ class Simulation(mp.Simulation):
             else:
                 local_index = self.background_index + mod_ridges   * (1- mpo.sin(theta+tilt)**8)  * (self.grating_index < self.background_index)
 
-        local_index += (np.random.rand(1) - .5)*0.05
+        local_index += (np.random.rand(1) - .5)*0.1
         return local_index**2 if local_index > 1 else 1
 
     def imported_structure(self, pos):
