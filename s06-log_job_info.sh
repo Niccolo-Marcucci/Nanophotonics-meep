d4f5b832d010e9ee271ffaea02ce47c02308d61d
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2022-09-19 21:32:01 +0200

    improve prefix extraction

--- s06-log_job_info.sh
+++ s06-log_job_info.sh
@@ -3,9 +3,14 @@
 jobid=$1
 
 log_file=$(ls *${jobid}.log)
-
-prefix=${log_file:0:-11} 	    # jobid + extension should take 11 characters
+if [ ".log" != "${log_file:$(( ${#log_fi} - 4 ))}" ]; then
+    exit
+fi
+prefix=${log_file%_${jobid}.log}    # extract prefix
 hash=${prefix:$(( ${#pref} - 10 ))} # then the hash is 10 chars
+
+echo $hash
+echo $prefix
 
 seff $jobid > ${log_file:0:-4}.info
 
