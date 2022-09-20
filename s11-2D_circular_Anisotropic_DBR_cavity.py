0f198e254a73471c04b5c0ab5f27e90dc9c7be65
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2022-09-20 15:24:40 +0200

    local run: file s11-2D_circular_Anisotropic_DBR_cavity.py. The prefix for the simulation is the hash of this commit

--- s11-2D_circular_Anisotropic_DBR_cavity.py
+++ s11-2D_circular_Anisotropic_DBR_cavity.py
@@ -432,7 +432,7 @@ def run_parallel(wavelength, n_eff_h, n_eff_l, n_eff_spacer, D, DBR_period, empt
     step_functions = []
     if sim.harminv_instance != None :
         step_functions.append( mp.after_time(150, sim.harminv_instance) )
-    step_functions.append(mp.at_every(0.025,save_fields))
+    step_functions.append(mp.at_every(0.05,save_fields))
     sim.run(*step_functions, until=sim_end)
     if df == 0 :
         sim.run(save_fields, until=1/f * 5 ) # an integer number of periods
